b2ad96e3b6c27938eed312ac4c8304a86da33bba
IvanK355 <sergan165@mail.ru>
2020-09-02 10:45:38 +0300

    Test1 ready

--- HomeTask2/Test1.java
+++ HomeTask2/Test1.java
@@ -1,3 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Test1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int s = Integer.parseInt(reader.readLine());
+        int lastdigit =  s%10;
+        System.out.println(lastdigit);
+    }
 
 }
