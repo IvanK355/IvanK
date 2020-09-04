7d8d267744af76fcc9acbc854b0c37e348785b37
IvanK355 <sergan165@mail.ru>
2020-09-04 14:56:33 +0300

    Выполнил пункты 1 и 2 тестового задания 1

--- HomeTask3/src/Test1/Main.java
+++ HomeTask3/src/Test1/Main.java
@@ -3,5 +3,23 @@ package Test1;
 public class Main {
     public static void main(String[] args) {
 
+        Phone iphone = new Phone();
+        iphone.number = 7;
+        iphone.model = "SE";
+        iphone.weight = 101.56;
+
+        Phone samsung = new Phone();
+        samsung.number = 9;
+        samsung.model = "Galaxy";
+        samsung.weight = 99.45;
+
+        Phone xiaomi = new Phone();
+        xiaomi.number = 8;
+        xiaomi.model = "Note";
+        xiaomi.weight = 104.45;
+
+        System.out.println(iphone.number+" "+iphone.model+" "+iphone.weight);
+        System.out.println(samsung.number+" "+samsung.model+" "+samsung.weight);
+        System.out.println(xiaomi.number+" "+xiaomi.model+" "+xiaomi.weight);
     }
 }
