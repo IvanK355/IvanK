f2a88e7692a70d057f2bbf4a8ccc1c68e58e847c
IvanK355 <sergan165@mail.ru>
2020-09-04 17:14:17 +0300

    Task2 vet

--- HomeTask3/src/Test2/Animal.java
+++ HomeTask3/src/Test2/Animal.java
@@ -1,18 +1,44 @@
 package Test2;
 
 public class Animal {
+    String food;
+    String location;
 
-}
+    void makeNoise(){
+        System.out.println("животное спит");
+    }
+    void eat(){}
+    void sleep(){}
+    }
 
 class Dog extends Animal{
 
+    @Override
+    void makeNoise() {
+        System.out.println("Dog животное спит");
+    }
 }
 
 class Cat extends Animal{
 
+    @Override
+    void makeNoise(){
+        System.out.println("Cat животное спит");
+    }
 }
 
 class Horse extends Animal{
 
+    @Override
+    void makeNoise(){
+        System.out.println("Horse животное спит");
+    }
+}
+
+class Veterinar{
+
+    void treatAnimal(Animal animal){
+
+    }
 }
 
