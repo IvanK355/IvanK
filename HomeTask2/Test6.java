2abc734519511f2955f5e763545707d36373ae80
IvanK355 <sergan165@mail.ru>
2020-09-07 09:04:56 +0300

    исправил 6 задаение

--- HomeTask2/Test6.java
+++ HomeTask2/Test6.java
@@ -4,12 +4,28 @@ import java.io.InputStreamReader;
 
 public class Test6 {
     public static void main(String[] args) throws IOException {
+        String plusminus;
+        String chetnecet;
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int s = Integer.parseInt(reader.readLine());
-        if ((s > 0) && ((s % 2 != 0))) System.out.println("положительное нечетное число");
-        else if ((s < 0) && ((s % 2 != 0))) System.out.println("отрицательное нечетное число");
-        else if ((s > 0) && ((s % 2 == 0))) System.out.println("положительное четное число");
-        else if ((s < 0) && ((s % 2 == 0))) System.out.println("отрицательное четное число");
-        else if (s == 0) System.out.println("нулевое число");
+
+        if (s > 0) {
+            plusminus = "положительное";
+        } else {
+            plusminus = "отрицательное";
+        }
+
+        if (s % 2 == 0) {
+            chetnecet = "четное";
+        } else {
+            chetnecet = "нечетное";
+        }
+
+        if (s == 0) {
+            System.out.println("нулевое число");
+        } else {
+            System.out.println(s + " " + plusminus + " " + chetnecet + " число");
+        }
     }
 }
