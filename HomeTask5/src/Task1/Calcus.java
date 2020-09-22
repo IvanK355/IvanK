473bef1d32c01aa8782a3025d240c8255539aa56
IvanK355 <sergan165@mail.ru>
2020-09-22 10:48:23 +0300

    5 DZ final - оказалось не так сложно )

--- HomeTask5/src/Task1/Calcus.java
+++ HomeTask5/src/Task1/Calcus.java
@@ -1,4 +1,5 @@
 package Task1;
+
 import java.util.Scanner;
 
 public class Calcus {
@@ -9,17 +10,18 @@ public class Calcus {
         System.out.println();
         System.out.print("Введите выражение и нажмите [Enter]:");
         Scanner sc = new Scanner(System.in);
+
         int a;
         int b;
         String c;
         do {
             String input = sc.nextLine();
-            input = input.replaceAll(" ","");
+            input = input.replaceAll(" ", "");
             if (input.equals("exit")) {
                 System.out.println("До свидания!");
                 break;
             }
-            String[] digit= input.split("\\W");
+            String[] digit = input.split("\\W");
             String[] operation = input.split("\\w");
             try {
                 a = Integer.parseInt(digit[0]);
@@ -30,15 +32,49 @@ public class Calcus {
                 System.out.println("Ошибка \"Неверный тип данных\": не удалось произвести расчет");
             }
         } while (true);
+
     }
 
     private static int calculation(int a, int b, String c) {
-        return switch (c) {
-            case "*" -> a * b;
-            case "+" -> a + b;
-            case "-" -> a - b;
-            case "/" -> a / b;
-            default -> 0;
-        };
+        int d;
+        String s = null;
+        LoggerFactory factory = null;
+        switch (c) {
+            case "*": {
+                factory = new ConsoleLoggerFactory();
+                s = "Умножили " + a + " на " + b;
+                d = a * b;
+                break;
+            }
+            case "+": {
+                factory = new DbLoggerFactory();
+                s = "Суммировали " + a + " и " + b;
+                d = a + b;
+                break;
+            }
+            case "-": {
+                factory = new FileLoggerFactory();
+                s = "Вычитание от " + a + " " + b;
+                d = a - b;
+                break;
+            }
+            case "/": {
+                factory = new ConsoleLoggerFactory();
+                s = "Разделили " + a + " на " + b;
+                d = a / b;
+                break;
+            }
+            default: {
+                factory = new ConsoleLoggerFactory();
+                s = "Не было никакой операции";
+                d = 0;
+                break;
+            }
+        }
+        if(factory != null){
+            Logger logger = factory.creatLogger();
+            logger.log(s);
+        }
+        return d;
     }
 }
