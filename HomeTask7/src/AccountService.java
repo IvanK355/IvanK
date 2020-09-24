acd85a6a60808ce270aa4e8dcd946313028d10f5
IvanK355 <sergan165@mail.ru>
2020-09-24 13:15:51 +0300

    ДЗ 7 v3

--- HomeTask7/src/AccountService.java
+++ HomeTask7/src/AccountService.java
@@ -1,12 +1,22 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class AccountService {
     static void withdraw(int accountId, int amount) throws
             NotEnoughMoneyException, UnknownAccountException {
 
     }
 
-    static void balance(int accountId) throws UnknownAccountException {
+    static void balance(int accountId) throws UnknownAccountException, IOException {
+        if (accountId < 1 && accountId > 10) {
+            throw new UnknownAccountException("Номер счета неверный");
+        }
         System.out.println("Баланс Счета: " + accountId);
-        System.out.println("1000 USD");
+        Files.lines(Paths.get("HomeTask7\\" + accountId + ".txt"), UTF_8).forEach(System.out::println);;
     }
 
     static void deposit(int accountId, int amount) throws
--- HomeTask7/src/Main.java
+++ HomeTask7/src/Main.java
@@ -1,36 +1,59 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException, UnknownAccountException, NotEnoughMoneyException, UnknownNameOperationException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int accountId, to, from, amount;
+        int qtyAccounts = 10;
+
+
+
+        try {
+            // Create new file
+            //String content = "100";
+            for (int i = 1; i < (qtyAccounts + 1); i++) {
+
+                String path = "HomeTask7\\"+i + ".txt";
+                File file = new File(path);
+                // If file doesn't exists, then create it
+                if (!file.exists()) {
+                    file.createNewFile();
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+
         String s = reader.readLine();
         String[] array = s.split("\\W");
 
         for (String x : array) {
             System.out.println(x);
-
         }
+
         switch (array[0]) {
             case ("balance") -> {
                 accountId = Integer.parseInt(array[1]);
-                AccountService.balance(accountId);}
+                AccountService.balance(accountId);
+            }
             case ("withdraw") -> {
                 accountId = Integer.parseInt(array[1]);
                 amount = Integer.parseInt(array[2]);
-                AccountService.withdraw(accountId, amount);}
+                AccountService.withdraw(accountId, amount);
+            }
             case ("deposite") -> {
                 accountId = Integer.parseInt(array[1]);
                 amount = Integer.parseInt(array[2]);
-                AccountService.deposit(accountId, amount);}
+                AccountService.deposit(accountId, amount);
+            }
             case ("transfer") -> {
                 from = Integer.parseInt(array[1]);
                 to = Integer.parseInt(array[2]);
                 amount = Integer.parseInt(array[3]);
-                AccountService.transfer(from, to, amount);}
+                AccountService.transfer(from, to, amount);
+            }
             default -> throw new UnknownNameOperationException("Неизвестная операция " + array[0]);
         }
     }
