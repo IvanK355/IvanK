9abe462b3bab7513aa93c4db7b55b40859d4f944
IvanK355 <sergan165@mail.ru>
2020-09-24 11:06:18 +0300

    ДЗ 7 v1

--- HomeTask7/src/Main.java
+++ HomeTask7/src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        String s = reader.readLine();
+        list.add(s);
+        String[] array = list.toArray(new String[0]);
+
+        switch (array[0]) {
+            case ("balance") -> System.out.println("Баланс");
+            case ("withdraw") -> System.out.println("Снятие");
+            case ("deposite") -> System.out.println("Пополнение");
+            case ("transfer") -> System.out.println("Перевод");
+        }
     }
 }
