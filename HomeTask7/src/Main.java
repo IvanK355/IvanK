9a8fbaeaf7a67efd98b64076213be6480c790c95
IvanK355 <sergan165@mail.ru>
2020-09-24 22:34:28 +0300

    ДЗ 7 v10

--- HomeTask7/src/Main.java
+++ HomeTask7/src/Main.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException, UnknownNameOperationException, UnknownAccountException, NotEnoughMoneyException {
@@ -9,9 +10,9 @@ public class Main {
         int accountId, to, from, amount;
         int qtyAccounts = 11;
 
-        Account[] accounts = new Account[qtyAccounts + 1];
+        ArrayList<Account> accounts = new ArrayList<>();
 
-        for (int i = 1; i < (qtyAccounts + 1); i++) {
+        for (int i = 1; i < 11; i++) {
 
             String path = "HomeTask7\\" + i + ".txt";
             File file = new File(path);
@@ -38,7 +39,7 @@ public class Main {
             bw.write("\n" + "Holder_1");
             bw.close();
 
-            for (int i = 2; i < (qtyAccounts + 1); i++) {
+            for (int i = 2; i < 22; i+=2) {
                 BufferedWriter writer = Files.newBufferedWriter(Path.of(path1), StandardOpenOption.APPEND);
                 writer.write("\n" + i);
                 writer.write("\n" + "Holder_" + i);
@@ -48,12 +49,14 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new FileReader(path1));
 
-        for (int i = 1; i < (qtyAccounts + 1); i++) {
-            accounts[i] = new Account(Integer.parseInt(br.readLine()), br.readLine());
+        //for (int i = 1; i < (qtyAccounts + 1); i++) {
+        String s1;
+        while ((s1 = br.readLine()) != null) {
+            accounts.add(new Account(Integer.parseInt(s1), br.readLine()));
         }
 
 
-          //  System.out.println(accounts[11].getHolder()+" " + accounts[11].getId());
+        //System.out.println(accounts.get(10).getHolder()+" " + accounts.get(10).getId());
 
 
         String s = reader.readLine();
@@ -63,54 +66,68 @@ public class Main {
 
         switch (array[0]) {
             case ("balance") -> {
-                accountId = Integer.parseInt(array[1]);
-                System.out.println("Счет: " + accounts[accountId].getId());
-                System.out.println("Владелец счета: " + accounts[accountId].getHolder());
-                System.out.print("Баланс счета: ");
-                System.out.println(accounts[accountId].getAmount());
+                for (int i = 0; i < accounts.size(); i++) {
+                    if (accounts.get(i).getId() == Integer.parseInt(array[1])) {
+                        System.out.println("Счет: " + accounts.get(i).getId());
+                        System.out.println("Владелец счета: " + accounts.get(i).getHolder());
+                        System.out.print("Баланс счета: ");
+                        System.out.println(accounts.get(i).getAmount());
+                    }
+                }
             }
             case ("withdraw") -> {
-                accountId = Integer.parseInt(array[1]);
-                amount = Integer.parseInt(array[2]);
-                System.out.println("Счет: " + accounts[accountId].getId());
-                System.out.println("Владелец счета: " + accounts[accountId].getHolder());
-                System.out.print("Баланс счета до снятия наличных: ");
-                System.out.println(accounts[accountId].getAmount());
-                System.out.println("Сумма снятия: " + amount);
-                AccountService.withdraw(accountId, amount);
-                System.out.print("Баланс счета после снятия наличных: ");
-                System.out.println(accounts[accountId].getAmount());
+                for (int i = 0; i < accounts.size(); i++) {
+                    if (accounts.get(i).getId() == Integer.parseInt(array[1])) {
+                        amount = Integer.parseInt(array[2]);
+                        System.out.println("Счет: " + accounts.get(i).getId());
+                        System.out.println("Владелец счета: " + accounts.get(i).getHolder());
+                        System.out.print("Баланс счета до снятия наличных: ");
+                        System.out.println(accounts.get(i).getAmount());
+                        System.out.println("Сумма снятия: " + amount);
+                        AccountService.withdraw(accounts.get(i).getId(), amount);
+                        System.out.print("Баланс счета после снятия наличных: ");
+                        System.out.println(accounts.get(i).getAmount());
+                    }
+                }
             }
             case ("deposit") -> {
-                accountId = Integer.parseInt(array[1]);
-                amount = Integer.parseInt(array[2]);
-                System.out.println("Счет: " + accounts[accountId].getId());
-                System.out.println("Владелец счета: " + accounts[accountId].getHolder());
-                System.out.print("Баланс счета до внесения наличных: ");
-                System.out.println(accounts[accountId].getAmount());
-                System.out.println("Сумма депозита: " + amount);
-                AccountService.deposit(accountId, amount);
-                System.out.print("Баланс счета после внесенмя наличных: ");
-                System.out.println(accounts[accountId].getAmount());
+                for (int i = 0; i < accounts.size(); i++) {
+                    if (accounts.get(i).getId() == Integer.parseInt(array[1])) {
+                        amount = Integer.parseInt(array[2]);
+                        System.out.println("Счет: " + accounts.get(i).getId());
+                        System.out.println("Владелец счета: " + accounts.get(i).getHolder());
+                        System.out.print("Баланс счета до внесения наличных: ");
+                        System.out.println(accounts.get(i).getAmount());
+                        System.out.println("Сумма депозита: " + amount);
+                        AccountService.deposit(accounts.get(i).getId(), amount);
+                        System.out.print("Баланс счета после внесенмя наличных: ");
+                        System.out.println(accounts.get(i).getAmount());
+                    }
+                }
             }
             case ("transfer") -> {
-                from = Integer.parseInt(array[1]);
-                to = Integer.parseInt(array[2]);
-                amount = Integer.parseInt(array[3]);
-                System.out.print("Баланс счета Отправителя " + accounts[from].getHolder() + " до перевода: ");
-                System.out.println(accounts[from].getAmount());
-                System.out.print("Баланс счета Получателя " + accounts[to].getHolder() + " до перевода: ");
-                System.out.println(accounts[to].getAmount());
-                System.out.println("Сумма перевода: " + amount);
-                AccountService.transfer(from, to, amount);
-                System.out.print("Баланс счета Отправителя " + accounts[from].getHolder() + " после перевода: ");
-                System.out.println(accounts[from].getAmount());
-                System.out.print("Баланс счета Получателя " + accounts[to].getHolder() + " после перевода: ");
-                System.out.println(accounts[to].getAmount());
+                for (int i = 0; i < accounts.size(); i++) {
+                    if (accounts.get(i).getId() == Integer.parseInt(array[1])) {
+                        for (int j = 0; j < accounts.size(); j++) {
+                            if (accounts.get(j).getId() == Integer.parseInt(array[2])) {
+                                amount = Integer.parseInt(array[3]);
+                                System.out.print("Баланс счета Отправителя " + accounts.get(i).getHolder() + " до перевода: ");
+                                System.out.println(accounts.get(i).getAmount());
+                                System.out.print("Баланс счета Получателя " + accounts.get(j).getHolder() + " до перевода: ");
+                                System.out.println(accounts.get(j).getAmount());
+                                System.out.println("Сумма перевода: " + amount);
+                                AccountService.transfer(Integer.parseInt(array[1]), Integer.parseInt(array[2]), amount);
+                                System.out.print("Баланс счета Отправителя " + accounts.get(i).getHolder() + " после перевода: ");
+                                System.out.println(accounts.get(i).getAmount());
+                                System.out.print("Баланс счета Получателя " + accounts.get(j).getHolder() + " после перевода: ");
+                                System.out.println(accounts.get(j).getAmount());
+                            }
+                        }
+                    }
+                }
+
             }
             default -> throw new UnknownNameOperationException("Неизвестная операция " + array[0]);
         }
-
-
     }
 }
