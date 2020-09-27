c3dceb6153c9efbbbd541ee69d855e23d3f6b61f
Ivan.Kiryukhin <ivan.Kiryukhin@x5.ru>
2020-09-27 11:02:57 +0300

    HomeTask8 v1 Interface create JBDC and Connection

--- HomeTask8/src/main/java/Main.java
+++ HomeTask8/src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+
+
     public static void main(String[] args) {
         try {
             Class.forName("org.h2.Driver");
+            try {
+                Connection connection = DriverManager.getConnection("jdbc:h2:mem:test");
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
         } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
         }
     }
 }
+
