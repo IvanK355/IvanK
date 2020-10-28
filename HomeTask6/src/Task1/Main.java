d2650d58e86c7a600fd21ab7832177cae8ed2251
Ivan.Kiryukhin <sergan165@mail.ru>
2020-10-28 15:02:18 +0300

    Task 6 updated v2

--- HomeTask6/src/Task1/Main.java
+++ HomeTask6/src/Task1/Main.java
@@ -10,16 +10,17 @@ public class Main {
 
         // initial array of size 10
         Object arr[]
-                = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                = { 1, 50, 2, 3, 4, 50, 5, 6, 7, 8, 9, 10 };
 
         // print the original array
         System.out.println("Initial Array:\n"
                 + Arrays.toString(arr));
 
         // element to be added
-        int x = 50;
+        Object x = 50;
 
         // call the method to add x in arr
+        arr = myArrayList.add(arr, x);
         arr = myArrayList.add(arr, x);
 
         // print the updated array
@@ -28,6 +29,42 @@ public class Main {
                 + Arrays.toString(arr));
 
         System.out.println(myArrayList.size(arr));
+
+        // Print the resultant array
+        System.out.println("Original Array: "
+                + Arrays.toString(arr));
+
+        // Get the specific index
+        int index = 2;
+
+        // Print the index
+        System.out.println("Index to be removed: "
+                + index);
+
+        // Remove the element
+        arr = myArrayList.removeByIndex(arr, index);
+
+        // Print the resultant array
+        System.out.println("Resultant Array: "
+                + Arrays.toString(arr));
+
+
+        System.out.println("Original Array: "
+                + Arrays.toString(arr));
+
+        // Get the specific index
+        Object item = 50;
+
+        // Print the index
+        System.out.println("Item to be removed: "
+                + item);
+
+        // Remove the element
+        arr = myArrayList.removeByElement(arr, item);
+
+        // Print the resultant array
+        System.out.println("Resultant Array: "
+                + Arrays.toString(arr));
 
     }
 }
--- HomeTask6/src/Task1/MyArrayList.java
+++ HomeTask6/src/Task1/MyArrayList.java
@@ -2,29 +2,59 @@ package Task1;
 
 public class MyArrayList {
 
-   int size;
+    private static int size;
 
-   int size(Object[] obj){
-       size = obj.length;
-       return size;
-   }
+    public int size(Object[] obj) {
+        size = obj.length;
+        return size;
+    }
 
-    public static Object[] add(Object arr[], Object x)
-    {
+    public Object[] add(Object arr[], Object x) {
         int i;
 
-        // create a new array of size n+1
         Object newarr[] = new Object[arr.length + 1];
 
-        // insert the elements from
-        // the old array into the new array
-        // insert all elements till n
-        // then insert x at n+1
         for (i = 0; i < arr.length; i++)
             newarr[i] = arr[i];
 
         newarr[arr.length] = x;
 
         return newarr;
+    }
+
+    public Object[] removeByIndex(Object[] arr, int index) {
+        if (arr == null
+                || index < 0
+                || index >= arr.length) {
+
+            return arr;
+        }
+
+        Object[] newArray = new Object[arr.length - 1];
+
+        for (int i = 0, k = 0; i < arr.length; i++) {
+
+            if (i == index) {
+                continue;
+            }
+            newArray[k++] = arr[i];
+        }
+
+        return newArray;
+    }
+
+    public Object[] removeByElement(Object[] arr, Object item) {
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == item){
+                // shifting elements
+                for(int j = i; j < arr.length - 1; j++){
+                    arr[j] = arr[j+1];
+                }
+                break;
+            }
+        }
+
+        return arr;
     }
 }
