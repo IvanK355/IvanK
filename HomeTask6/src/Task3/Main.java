2519198a021de4f2eafebc641da943200805cd36
Ivan.Kiryukhin <sergan165@mail.ru>
2020-10-28 22:54:20 +0300

    Task 6 part 2 MyLinkedList updated v2

--- HomeTask6/src/Task3/Main.java
+++ HomeTask6/src/Task3/Main.java
@@ -1,7 +1,28 @@
 package Task3;
 
+import Task1.MyArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        MyLinkedList<String> arr = new MyLinkedList<>();
+
+        arr.add("1");
+        arr.add("2");
+        arr.add("50");
+        arr.add("3");
+        arr.add("4");
+        arr.add("5");
+        arr.add("6");
+        arr.add("7");
+        arr.add("8");
+        arr.add("9");
+
+
+        System.out.println("Initial size:" + arr.size());
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.print(arr.get(i) + " ");
+        }
 
     }
 }
--- HomeTask6/src/Task3/MyLinkedList.java
+++ HomeTask6/src/Task3/MyLinkedList.java
@@ -1,4 +1,62 @@
 package Task3;
 
-public class MyLinkedList {
+public class MyLinkedList<E> {
+    int size = 0;
+
+    MyLinkedList.Node<E> first;
+    MyLinkedList.Node<E> last;
+
+    private static class Node<E> {
+        E item;
+        MyLinkedList.Node<E> next;
+        MyLinkedList.Node<E> prev;
+
+        Node(MyLinkedList.Node<E> prev, E element, MyLinkedList.Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    void linkLast(E e) {
+        final MyLinkedList.Node<E> l = last;
+        final MyLinkedList.Node<E> newNode = new MyLinkedList.Node<>(l, e, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+
+    public boolean add(E e) {
+        linkLast(e);
+        return true;
+    }
+
+    public E get(int index) {
+
+        return node(index).item;
+    }
+
+    MyLinkedList.Node<E> node(int index) {
+
+        if (index < (size >> 1)) {
+            MyLinkedList.Node<E> x = first;
+            for (int i = 0; i < index; i++)
+                x = x.next;
+            return x;
+        } else {
+            MyLinkedList.Node<E> x = last;
+            for (int i = size - 1; i > index; i--)
+                x = x.prev;
+            return x;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+
 }
