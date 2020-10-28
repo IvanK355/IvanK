89583f19a771cd24e69b5f694f920f1f056ec9cb
Ivan.Kiryukhin <sergan165@mail.ru>
2020-10-28 19:14:31 +0300

    Task 6 part MyArrayList updated v4

--- HomeTask6/src/Task1/Main.java
+++ HomeTask6/src/Task1/Main.java
@@ -1,72 +1,19 @@
 package Task1;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        MyArrayList myArrayList = new MyArrayList();
+        MyArrayList arr = new MyArrayList();
 
-        Object arr[] = {1, 50, 2, 3, 4, 50, 5, 6, 7, 8, 9, 10};
-
-        System.out.println("Initial Array size:\n"+
-                myArrayList.size(arr));
-
-        System.out.println("Initial Array:\n"
-                + Arrays.toString(arr));
-
-        Object x = 50;
-
-        arr = myArrayList.add(arr, x);
-        arr = myArrayList.add(arr, x);
-
-        System.out.println("\nArray with " + x
-                + " added:\n"
-                + Arrays.toString(arr));
-
-        System.out.println("Original Array: "
-                + Arrays.toString(arr));
-
-        int index = 2;
-
-        System.out.println("Index to be removed: "
-                + index);
-
-        arr = myArrayList.removeByIndex(arr, index);
-
-        System.out.println("Resultant Array: "
-                + Arrays.toString(arr));
+        arr.add(1);
+        arr.add(2);
 
 
-        System.out.println("Original Array: "
-                + Arrays.toString(arr));
+        System.out.println("Initial size:\n"
+                + arr.size());
+        for(int i=0;i< arr.size();i++) {
+            System.out.print(arr.get(i)+" ");
+        }
 
-          Object item = 50;
-
-        System.out.println("Item to be removed: "
-                + item);
-
-        arr = myArrayList.removeByElement(arr, item);
-
-        System.out.println("Resultant Array: "
-                + Arrays.toString(arr));
-
-        index = 5;
-
-        System.out.println("Index to be shown: "
-                + index);
-
-        Object itemByIndex = myArrayList.get(arr, index);
-
-        System.out.println("Resultant item: "
-                + itemByIndex);
-
-        System.out.println("Original Array: "
-                + Arrays.toString(arr));
-
-        arr = myArrayList.clear(arr);
-
-        System.out.println("Resultant Array: "
-                + Arrays.toString(arr));
 
     }
 }
--- HomeTask6/src/Task1/MyArrayList.java
+++ HomeTask6/src/Task1/MyArrayList.java
@@ -1,70 +1,33 @@
 package Task1;
 
-public class MyArrayList {
+import java.util.Objects;
 
-    public int size(Object[] arr) {
-        return arr.length;
+public class MyArrayList<E> {
+
+    private int size = 0;
+    private Object[] elementData;
+
+    public MyArrayList() {
+        this.elementData = new Object[10];
     }
 
-    public Object[] add(Object arr[], Object x) {
-        int i;
+    private void add(E e, Object[] elementData, int s) {
+        elementData[s] = e;
+        size = s + 1;
 
-        Object newarr[] = new Object[arr.length + 1];
-
-        for (i = 0; i < arr.length; i++)
-            newarr[i] = arr[i];
-
-        newarr[arr.length] = x;
-
-        return newarr;
     }
 
-    public Object[] removeByIndex(Object[] arr, int index) {
-        if (arr == null
-                || index < 0
-                || index >= arr.length) {
-
-            return arr;
-        }
-
-        Object[] newArray = new Object[arr.length - 1];
-
-        for (int i = 0, k = 0; i < arr.length; i++) {
-
-            if (i == index) {
-                continue;
-            }
-            newArray[k++] = arr[i];
-        }
-
-        return newArray;
+    public boolean add(E e) {
+        add(e, elementData, size);
+        return true;
     }
 
-    public Object[] removeByElement(Object[] arr, Object item) {
-
-
-        int count = 0;
-        for (Object o : arr) {
-            if (o == item)
-                count++;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == item) {
-                if (arr.length - 1 - i >= 0) System.arraycopy(arr, i + 1, arr, i, arr.length - 1 - i);
-            }
-        }
-        Object[] newArray = new Object[arr.length - count];
-
-        if (arr.length - count >= 0) System.arraycopy(arr, 0, newArray, 0, arr.length - count);
-
-        return newArray;
+    public E get(int index) {
+        return (E) elementData[index];
     }
 
-    public Object get (Object[] arr, int index) {
-           return arr[index];
+    public int size() {
+        return size;
     }
 
-    public Object[] clear(Object[] arr) {
-        return arr = null;
-    }
 }
